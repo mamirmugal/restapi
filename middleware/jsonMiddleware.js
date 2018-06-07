3550760d6931a14483f368067c5ff45e65419d1d
muhammad <Muhammad Amir>
2018-06-08 00:25:21 +1000

    fixed homepage

--- middleware/jsonMiddleware.js
+++ middleware/jsonMiddleware.js
@@ -4,11 +4,11 @@ module.exports = function (req, res, next) {
     var requestType = req.get('Content-Type');
 
     // only allowing json to pass
-    if (requestType == 'application/json') {
+    if (requestType == 'application/json' || req.accepts('html')) {
         next();
     } else {
         res.status(400).send({
-            error: "Could not decode request: JSON parsing failed"
+            error: "Could not decode request: JSON parsing failed!"
         });
     }
 };
